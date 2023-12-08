919c8a7137d6fb4ebaba632d4df7556c0e3ca951
maple@max <feng.liang@kaust.edu.sa>
2023-12-08 16:19:03 +0800

    update run file to be used in container

--- run_comfyui.sh
+++ run_comfyui.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/zsh
 
 # Default values
-CUDA_DEVICE=""
-EXPECTED_PYTHON_PATH="/home/ai/mambaforge/envs/comfyui/bin/python"
+CUDA_DEVICE="0"
+EXPECTED_PYTHON_PATH="/home/jenson/miniforge3/bin/python"
+PORT=""
+
+# Function to validate if a string is an integer
+is_valid_integer() {
+    [[ "$1" =~ ^[0-9]+$ ]]
+}
 
 # Parse command-line options
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --gpu)
-            CUDA_DEVICE="$2"
-            shift 2
+        --port)
+            if is_valid_integer "$2"; then
+                PORT="$2"
+                shift 2
+            else
+                echo "Error: Invalid port number specified."
+                exit 1
+            fi
             ;;
         *)
             echo "Invalid option: $1"
@@ -18,14 +29,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Check if required options are provided
-if [ -z "$CUDA_DEVICE" ] ; then
-    echo "Error: Required options --gpu are missing."
-    exit 1
-fi
-
-
-source /home/ai/mambaforge/bin/activate comfyui
+source /home/jenson/miniforge3/bin/activate
 
 # Check python path
 current_python_path=$(which python)
@@ -35,10 +39,10 @@ if [ "$current_python_path" != "$EXPECTED_PYTHON_PATH" ]; then
 fi
 
 # Set proxy
-export http_proxy="http://localhost:9910" https_proxy="http://localhost:9910" HTTP_PROXY="http://localhost:9910" HTTPS_PROXY="http://localhost:9910"
+export http_proxy="http://host.docker.internal:9910" https_proxy="http://host.docker.internal:9910" HTTP_PROXY="http://host.docker.internal:9910" HTTPS_PROXY="http://host.docker.internal:9910"
 echo "Using proxy ${http_proxy}"
 
 # Set TensorRT
-export LD_LIBRARY_PATH="/home/ai/mambaforge/envs/comfyui/lib/python3.10/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/home/jenson/miniforge3/lib/python3.10/site-packages/tensorrt_libs:$LD_LIBRARY_PATH"
 
-python main.py --listen --port 8188 --cuda-device "$CUDA_DEVICE"
+python main.py --listen --port $PORT --cuda-device "$CUDA_DEVICE"
